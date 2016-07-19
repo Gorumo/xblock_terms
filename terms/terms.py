10d39e7ca432604b30b0b3ac4008f0a8ca707a5d
Dmitri Volchek <dvolchek@mail.ru>
2016-07-19 16:47:17 +0300

    22

--- terms/terms.py
+++ terms/terms.py
@@ -10,74 +10,73 @@ from xblock.fragment import Fragment
 
 
 class TermsXBlock(XBlock):
-    """
-    TO-DO: document what your XBlock does.
-    """
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
+	"""
+	TO-DO: document what your XBlock does.
+	"""
+	# Fields are defined on the class.	You can access them in your code as
+	# self.<fieldname>.
 
-    # TO-DO: delete count, and define your own fields.   
+	# TO-DO: delete count, and define your own fields.	 
 
-    arr = []
+	arr = []
 
-    exampleList = String(
-        default=0, scope=Scope.preferences,
-        help="field for concepts",
-    )
+	exampleList = String(
+		default=0, scope=Scope.preferences,
+		help="field for concepts",
+	)
 
-    def resource_string(self, path): 
-        """Handy helper for getting resources from our kit."""
-        data = pkg_resources.resource_string(__name__, path)
-        return data.decode("utf8")
+	def resource_string(self, path): 
+		"""Handy helper for getting resources from our kit."""
+		data = pkg_resources.resource_string(__name__, path)
+		return data.decode("utf8")
 
-    # TO-DO: change this view to display your data your own way.
-    def student_view(self, context=None):
-        """
-        The primary view of the TermsXBlock, shown to students
-        when viewing courses.
-        """
+	# TO-DO: change this view to display your data your own way.
+	def student_view(self, context=None):
+		"""
+		The primary view of the TermsXBlock, shown to students
+		when viewing courses.
+		"""
 
-        html = self.resource_string("static/html/terms.html")
-        frag = Fragment(html.format(self=self))
-        frag.add_css(self.resource_string("static/css/terms.css"))
-        frag.add_javascript(self.resource_string("static/js/src/terms.js"))
-        frag.initialize_js('TermsXBlock')
-        return frag
+		html = self.resource_string("static/html/terms.html")
+		frag = Fragment(html.format(self=self))
+		frag.add_css(self.resource_string("static/css/terms.css"))
+		frag.add_javascript(self.resource_string("static/js/src/terms.js"))
+		frag.initialize_js('TermsXBlock')
+		return frag
 
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def aad(self, data, suffix=''):
-        if self.exampleList != 0:
-            self.arr = json.loads(self.exampleList)
-        self.arr.append('yeah')
-        self.exampleList = json.dumps(self.arr)
-
-        return {"exampleList" : self.exampleList}
-	
+	# TO-DO: change this handler to perform your own actions.  You may need more
+	# than one handler, or you may not need any handlers at all.
 	@XBlock.json_handler
-    def termsListCheck(self, data, suffix=''):
-        if self.exampleList != 0:
-            self.arr = json.loads(self.exampleList)
-        self.arr.append(data.get('term'))
-        self.exampleList = json.dumps(self.arr)
+	def aad(self, data, suffix=''):
+		if self.exampleList != 0:
+			self.arr = json.loads(self.exampleList)
+		self.arr.append('yeah')
+		self.exampleList = json.dumps(self.arr)
 
-        return {"exampleList" : self.exampleList}
+		return {"exampleList" : self.exampleList}
+	@XBlock.json_handler
+	def termsListCheck(self, data, suffix=''):
+		if self.exampleList != 0:
+			self.arr = json.loads(self.exampleList)
+		self.arr.append(data.get('term'))
+		self.exampleList = json.dumps(self.arr)
 
-    # TO-DO: change this to create the scenarios you'd like to see in the
-    # workbench while developing your XBlock.
-    @staticmethod
-    def workbench_scenarios():
-        """A canned scenario for display in the workbench."""
-        return [
-            ("TermsXBlock",
-             """<terms/>
-             """),
-            ("Multiple TermsXBlock",
-             """<vertical_demo>
-                <terms/>
-                <terms/>
-                <terms/>
-                </vertical_demo>
-             """),
-        ]
+		return {"exampleList" : self.exampleList}
+
+	# TO-DO: change this to create the scenarios you'd like to see in the
+	# workbench while developing your XBlock.
+	@staticmethod
+	def workbench_scenarios():
+		"""A canned scenario for display in the workbench."""
+		return [
+			("TermsXBlock",
+			 """<terms/>
+			 """),
+			("Multiple TermsXBlock",
+			 """<vertical_demo>
+				<terms/>
+				<terms/>
+				<terms/>
+				</vertical_demo>
+			 """),
+		]

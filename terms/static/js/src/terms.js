1fd97a304cf1555a16f954bd8131d6fbda2ca43d
Aleksei Romanov <Gloomspb@gmail.com>
2016-07-20 15:58:11 +0300

    select

--- terms/static/js/src/terms.js
+++ terms/static/js/src/terms.js
@@ -1,32 +1,19 @@
 /* Javascript for TermsXBlock. */
 function TermsXBlock(runtime, element) {
 
-    function updateList(result) {
-        //Получили count и next_one из скрипта-обработчика increment_count
+    function updateCount(result) {
+        //Функция, которая берет из поля для ввода текстовую информацию и запихивает в массив exampleList
         $('.exampleListOfTerms', element).text(result.exampleList);
     }
 
-    //increment_ount - привязываем данный "элемент" к скрипту increment_count и генерируем URL
-    //var handler2 = runtime.handler2(element, 'aad');
+    var handlerUrl = runtime.handlerUrl(element, 'termsListCheck'); //increment_ount - привязываем данный "элемент" к скрипту increment_count и генерируем URL
+
     $('.halo', element).click(function(eventObject) {
-         var handlerUrl = runtime.handlerUrl(element, 'termsListCheck');
         $.ajax({
             type: "POST",
             url: handlerUrl,
             data: JSON.stringify({"term": document.getElementById("term").value}), //Запечатываем массив (hello => "world" и hello2 => "check")
-            success: updateList //Вызов функции updateList(result) при успехе, где result - объект, который мы передали из (П) функции increment_count
+            success: updateCount //Вызов функции updateCount(result) при успехе, где result - объект, который мы передали из (П) функции increment_count
         });
-    });
-  $('.ad', element).click(function(eventObject) {
-     var handlerUrl = runtime.handlerUrl(element, 'aad');
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"term": document.getElementById("term").value}),
-            success: updateList //Вызов функции updateList(result) при успехе, где result - объект, который мы передали из (П) функции increment_count
-        });
-    });
-    $(function ($) {
-        //Тут действия при загрузке страницы
     });
 }
--- terms/terms.py
+++ terms/terms.py
@@ -1,82 +1,91 @@
 """TO-DO: Write a description of what this XBlock is."""
 import json
 import pkg_resources
+import datetime
+import mysql.connector
 
 from xblock.core import XBlock
 from xblock.fields import Scope, Integer, String
 from xblock.fragment import Fragment
-
-
+from mysql.connector import errorcode
+import settings as s
 
 
 class TermsXBlock(XBlock):
-	"""
-	TO-DO: document what your XBlock does.
-	"""
-	# Fields are defined on the class.	You can access them in your code as
-	# self.<fieldname>.
+    """
+    TO-DO: document what your XBlock does.
+    """
+    # Fields are defined on the class.  You can access them in your code as
+    # self.<fieldname>.
 
-	# TO-DO: delete count, and define your own fields.	 
+    # TO-DO: delete count, and define your own fields.   
+    
+    arr = []
+    overall = []
 
-	arr = []
+    exampleList = String(
+        default=0, scope=Scope.settings,
+        help="shows next nuber",
+    )
+    test = String(
+        default=0, scope=Scope.settings,
+        help="shows next nuber",
+    )
 
-	exampleList = String(
-		default=0, scope=Scope.content,
-		help="field for concepts",
-	)
+    def resource_string(self, path): 
+        """Handy helper for getting resources from our kit."""
+        data = pkg_resources.resource_string(__name__, path)
+        return data.decode("utf8")
 
-	def resource_string(self, path): 
-		"""Handy helper for getting resources from our kit."""
-		data = pkg_resources.resource_string(__name__, path)
-		return data.decode("utf8")
 
-	# TO-DO: change this view to display your data your own way.
-	def student_view(self, context=None):
-		"""
-		The primary view of the TermsXBlock, shown to students
-		when viewing courses.
-		"""
+    # TO-DO: change this view to display your data your own way.
+    
+    def student_view(self, context=None):
+        """
+        The primary view of the TermsXBlock, shown to students
+        when viewing courses.
+        """
 
-		html = self.resource_string("static/html/terms.html")
-		frag = Fragment(html.format(self=self))
-		frag.add_css(self.resource_string("static/css/terms.css"))
-		frag.add_javascript(self.resource_string("static/js/src/terms.js"))
-		frag.initialize_js('TermsXBlock')
-		return frag
+        html = self.resource_string("static/html/terms.html")
+        frag = Fragment(html.format(self=self))
+        frag.add_css(self.resource_string("static/css/terms.css"))
+        frag.add_javascript(self.resource_string("static/js/src/terms.js"))
+        frag.initialize_js('TermsXBlock')
+        return frag
 
-	# TO-DO: change this handler to perform your own actions.  You may need more
-	# than one handler, or you may not need any handlers at all.
-	@XBlock.json_handler
-	def aad(self, data, suffix=''):
-		if self.exampleList != 0:
-			self.arr = json.loads(self.exampleList)
-		self.arr.append('yeah')
-		self.exampleList = json.dumps(self.arr)
+    # TO-DO: change this handler to perform your own actions.  You may need more
+    # than one handler, or you may not need any handlers at all.
+    @XBlock.json_handler
+    def termsListCheck(self, data, suffix=''):
+        if self.exampleList != 0:
+            self.arr = json.loads(self.exampleList)
+        self.new_term = data.get('term')
+        self.arr.append(self.new_term)
+        self.exampleList = json.dumps(self.arr)
+        cnx = mysql.connector.connect(**s.database)
+        cursor = cnx.cursor()
+        cursor.execute("select id from allTerms where term = '123'")
+        data = cursor.fetchall()
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        self.test = json.dumps(data)
+        return {"exampleList" : self.exampleList ,"test" :self.test} 
 
-		return {"exampleList" : self.exampleList}
-	@XBlock.json_handler
-	def termsListCheck(self, data, suffix=''):
-		if self.exampleList != 0:
-			self.arr = json.loads(self.exampleList)
-		self.arr.append(data.get('term'))
-		self.exampleList = json.dumps(self.arr)
-
-		return {"exampleList" : self.exampleList}
-
-	# TO-DO: change this to create the scenarios you'd like to see in the
-	# workbench while developing your XBlock.
-	@staticmethod
-	def workbench_scenarios():
-		"""A canned scenario for display in the workbench."""
-		return [
-			("TermsXBlock",
-			 """<terms/>
-			 """),
-			("Multiple TermsXBlock",
-			 """<vertical_demo>
-				<terms/>
-				<terms/>
-				<terms/>
-				</vertical_demo>
-			 """),
-		]
+    # TO-DO: change this to create the scenarios you'd like to see in the
+    # workbench while developing your XBlock.
+    @staticmethod
+    def workbench_scenarios():
+        """A canned scenario for display in the workbench."""
+        return [
+            ("TermsXBlock",
+             """<terms/>
+             """),
+            ("Multiple TermsXBlock",
+             """<vertical_demo>
+                <terms/>
+                <terms/>
+                <terms/>
+                </vertical_demo>
+             """),
+        ]

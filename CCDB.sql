8d8497524ede9dafc066387d75b1bdaff6ceb85c
Aleksei Romanov <Gloomspb@gmail.com>
2016-07-28 11:19:45 +0300

    sql fix

--- CCDB.sql
+++ CCDB.sql
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS `Concepts` (
   `concept_label` varchar(120) CHARACTER SET latin1 NOT NULL,
   `concept_description` varchar(250) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`concept_id`),
-  UNIQUE KEY `concept_id` (`concept_id`)
+  UNIQUE KEY `concept_id` (`concept_id`),
+  UNIQUE KEY `concept_label` (`concept_label`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -39,6 +40,7 @@ CREATE TABLE IF NOT EXISTS `Fields` (
   `field_id` int(10) NOT NULL AUTO_INCREMENT,
   `field_label` varchar(120) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`field_id`),
-  UNIQUE KEY `field_id` (`field_id`)
+  UNIQUE KEY `field_id` (`field_id`),
+  UNIQUE KEY `field_label` (`field_label`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
